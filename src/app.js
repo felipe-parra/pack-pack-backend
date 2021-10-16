f9e5d55a0817d63cc6cb9632a9543f6f32645ac7
felipe parra <felipe.parra@magnotechnology.com>
2021-10-16 10:39:17 -0500

    [update] database, cors & logger

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
 const express = require("express");
 
+const path = require("path");
+
+const favicon = require("serve-favicon");
+
+const cors = require("cors");
+
+const config = require("./config");
+
 const routes = require("./routes");
 
+const logger = require("morgan");
+
+const { verifyAppToken } = require("./middlewares/verifyAppToken");
+
+require("./database");
+
 const app = express();
+app.use(logger("dev"));
+app.use(cors("*"));
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/favicon.ico", express.static("images/favicon.ico"));
+app.use(favicon(path.join(__dirname, "images", "favicon.ico")));
 
-app.use("/api", routes);
+app.use(`/api/${config.versionApi}/`, verifyAppToken, routes);
 
 module.exports = app;
