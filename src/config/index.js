ba2e0cfa84f593fc615640f7ca7a55b460ee4bd7
felipe parra <felipe.parra@magnotechnology.com>
2021-10-16 10:40:08 -0500

    [update] variables

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,17 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const config = {
-    port: process.env.PORT
-}
+  port: process.env.PORT,
+  versionApi: process.env.VERSION_API,
+  dbUriMongoAtlas: process.env.DB_URI_MONGO_ATLAS,
+  dbUriMongoLocal: process.env.DB_URI_MONGO_LOCAL,
+  dbMongoUser: process.env.DB_MONGO_USER,
+  dbMongoPass: process.env.DB_MONGO_PASS,
+  dbMongoName: process.env.DB_MONGO_NAME,
+  appSecret: process.env.APP_SECRET,
+  jwtSecret: String(process.env.JWT_SECRET),
+  genSalt: process.env.GEN_SALT,
+  timeExpiresToken: 86400,
+};
 
-module.exports = config
+module.exports = config;
